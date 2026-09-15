4630bc45e4d8e049b2f617f31ae13a71549f0e12
Fuad Naufal Ihsan <ifuadnaufal@gmail.com>
2026-09-15 15:05:21 +0700

    chore: restrukturisasi folder untuk Expo Router

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,18 @@
 // app/index.tsx
+import { useEffect, useState } from "react";
 import { View } from "react-native";
-import { useState, useEffect } from "react";
-import WeatherCard from "../../components/WeatherCard";
-import SearchBox from "../../components/SearchBox";
-import RiwayatList from "../../components/RiwayatList";
+import RiwayatList from "../../../components/RiwayatList";
+import SearchBox from "../../../components/SearchBox";
+import WeatherCard from "../../../components/WeatherCard";
 
 export default function HalamanUtama() {
   const [kotaAktif, setKotaAktif] = useState("Pekalongan");
   const [riwayat, setRiwayat] = useState<string[]>(["Pekalongan"]);
+
+  // tambahkan di dalam HalamanUtama, setelah deklarasi state:
+  useEffect(() => {
+    console.log("Kota aktif berubah menjadi:", kotaAktif);
+  }, [kotaAktif]);
 
   function handleCari(kota: string) {
     setKotaAktif(kota);
@@ -15,9 +20,9 @@ export default function HalamanUtama() {
       setRiwayat([...riwayat, kota]);
     }
   }
-  
+
   return (
-    <View style={{ padding: 16, gap: 16 }}>
+    <View style={{ padding: 16, paddingTop: 100, gap: 16 }}>
       <SearchBox onCari={handleCari} />
       <WeatherCard kota={kotaAktif} suhu={29} tingkatAQI="BAIK" />
       <RiwayatList daftarKota={riwayat} />
