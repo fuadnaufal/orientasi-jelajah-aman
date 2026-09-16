130db6c35681075b4e5f78d8844516a29530aa05
Fuad Naufal Ihsan <ifuadnaufal@gmail.com>
2026-09-17 00:48:08 +0700

    feat: buat komponen RiwayatList (update)

--- components/RiwayatList.tsx
+++ components/RiwayatList.tsx
@@ -1,5 +1,6 @@
 // components/RiwayatList.tsx
 import { View, Text } from "react-native";
+import { Link } from "expo-router";
 
 interface RiwayatListProps {
     daftarKota: string[];
@@ -9,7 +10,9 @@ export default function RiwayatList({ daftarKota }: RiwayatListProps) {
     return (
         <View>
             {daftarKota.map((kota) => (
-                <Text key={kota}>{kota}</Text>
+                <Link key={kota} href={{ pathname: "/detail/[kota]", params: { kota } }}>
+                    <Text key={kota}>{kota}</Text>
+                </Link>
             ))}
         </View>
     );
