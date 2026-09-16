81a50fad98dcd57538da774fded79e6e0e6ea85e
Fuad Naufal Ihsan <ifuadnaufal@gmail.com>
2026-09-17 00:47:45 +0700

    feat: buat komponen SearchBox dengan useState (update)

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -10,14 +10,14 @@ export default function SearchBox({ onCari }: SearchBoxProps) {
     const [teks, setTeks] = useState("");
 
     return (
-        <View style={{ flexDirection: "row", gap: 8 }}>
-            <TextInput
-                placeholder="Nama kota"
-                value={teks}
-                onChangeText={setTeks}
-                style={{ flex: 1, borderWidth: 1, padding: 8 }}
-            />
+    <View style={{ flexDirection: "row", gap: 8 }}>
+        <TextInput
+            placeholder="kota"
+            value={teks}
+            onChangeText={setTeks}
+            style={{ flex: 1, borderWidth: 1, padding: 8 }}
+        />
         <Button title="Cari" onPress={() => onCari(teks)} />
-        </View>
+    </View>
     );
 }
