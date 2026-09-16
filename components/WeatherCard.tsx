62dfd8e9019283287ddb9ffb00b8447b8e35ab0e
Fuad Naufal Ihsan <ifuadnaufal@gmail.com>
2026-09-17 00:47:23 +0700

    feat: buat komponen WeatherCard (update)

--- components/WeatherCard.tsx
+++ components/WeatherCard.tsx
@@ -1,15 +1,17 @@
 // components/WeatherCard.tsx
-import { View, Text } from "react-native";
+import { View, Text, } from "react-native";
 import { WeatherCardProps } from "../types/cuaca";
+import { typeScale, spacing } from "../src/constants/styles";
 
 export default function WeatherCard({ kota, suhu, tingkatAQI }: WeatherCardProps) {
     const warnaAQI = tingkatAQI === "BAIK" ? "green" : "orange";
     
     return (
-        <View style={{ padding: 16, borderRadius: 8, backgroundColor: "#F4F7FA" }}>
-            <Text style={{ fontWeight: "bold", fontSize: 18 }}>{kota}</Text>
+        // 2. Ganti angka padding menjadi spacing.sedang
+        <View style={{ padding: spacing.sedang, borderRadius: 8, backgroundColor: "#F4F7FA" }}>
+            <Text style={{ fontWeight: "bold", fontSize: typeScale.judul }}>{kota}</Text>
             <Text style={{ fontSize: 32 }}>{suhu}°C</Text>
-            <Text style={{ color: warnaAQI }}>AQI: {tingkatAQI}</Text>
+            <Text style={{ color: warnaAQI, fontSize: typeScale.isi }}>AQI:{tingkatAQI}</Text>
         </View>
     );
 }
